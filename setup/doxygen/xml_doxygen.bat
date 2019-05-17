538c26f7418c23adb36ded9f0a561c81258b2792
Carsten Arnholm <arnholm@arnholm.org>
2019-05-17 20:40:57 +0200

    Windows xml_doxygen.bat update

--- setup/doxygen/xml_doxygen.bat
+++ setup/doxygen/xml_doxygen.bat
@@ -1,37 +1,23 @@
+@echo off
+REM
+REM Generate AngelCAD documentation
+REM
+REM Create/update angelcad_xmldoc.xml and angelcad.h
 as_csg -xmldoc doc.as
+REM
+REM Run doxygen using angelcad.h as input
 doxygen Doxyfile_AngelCAD
-#
-# http://fart-it.sourceforge.net/
-# this call to fart will replace return types written as C++ pointers
-# into angelscript references
-# example:
-#  "pos2d * "  =>  "pos2d@ "
-#
-# fix return value
+REM
+REM Fix return types 
+REM    from C++ pointer          : "pos2d * "
+REM     to AngelScript reference : "pos2d@ "
+REM
+REM Using http://fart-it.sourceforge.net/
 fart  %CPDE_USR%\doc\AngelCAD\html\*.html " *&#160;" "@&#160;" 
-#
-# fix arrays
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classshape.html\">shape</a> @[]@" "array&lt<a class=\"el\" href=\"classshape.html\">shape</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classshape.html\">shape</a> @@[]" "array&lt<a class=\"el\" href=\"classshape.html\">shape</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classshape2d.html\">shape2d</a> @[]@" "array&lt<a class=\"el\" href=\"classshape2d.html\">shape2d</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classshape2d.html\">shape2d</a> @@[]" "array&lt<a class=\"el\" href=\"classshape2d.html\">shape2d</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classsolid.html\">solid</a> @[]@" "array&lt<a class=\"el\" href=\"classsolid.html\">solid</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classsolid.html\">solid</a> @@[]" "array&lt<a class=\"el\" href=\"classsolid.html\">solid</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpos2d.html\">pos2d</a> @[]@" "array&lt<a class=\"el\" href=\"classpos2d.html\">pos2d</a>@&gt@ "
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpos2d.html\">pos2d</a> @@[]" "array&lt<a class=\"el\" href=\"classpos2d.html\">pos2d</a>@&gt@ "
-# 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpos3d.html\">pos3d</a> @[]@" "array&lt<a class=\"el\" href=\"classpos3d.html\">pos3d</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpos3d.html\">pos3d</a> @@[]" "array&lt<a class=\"el\" href=\"classpos3d.html\">pos3d</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpface.html\">pface</a> @[]@" "array&lt<a class=\"el\" href=\"classpface.html\">pface</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classpface.html\">pface</a> @@[]" "array&lt<a class=\"el\" href=\"classpface.html\">pface</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classvec2d.html\">vec2d</a> @[]@" "array&lt<a class=\"el\" href=\"classvec2d.html\">vec2d</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classvec2d.html\">vec2d</a> @@[]" "array&lt<a class=\"el\" href=\"classvec2d.html\">vec2d</a>@&gt@ " 
-#
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classvec3d.html\">vec3d</a> @[]@" "array&lt<a class=\"el\" href=\"classvec3d.html\">vec3d</a>@&gt@ " 
-fart  %CPDE_USR%\doc\AngelCAD\html\*.html "<a class=\"el\" href=\"classvec3d.html\">vec3d</a> @@[]" "array&lt<a class=\"el\" href=\"classvec3d.html\">vec3d</a>@&gt@ " 
+REM 
+REM Because of the replace, run hhc.exe again
+echo Generating angelcad.chm (second run)
+"C:/Program Files (x86)/HTML Help Workshop/hhc.exe"  %CPDE_USR%\doc\AngelCAD\html\index.hhp
+echo ===============================
+echo AngeCAD documentation complete.
+echo ===============================
